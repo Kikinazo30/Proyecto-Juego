99e8cf0beec1ba58fb33541dc2585851fbd48bfa
EOBM666 <a25110222@ceti.mx>
2026-06-14 21:10:26 -0600

    Add attack functionality to Player class

--- include/Player.h
+++ include/Player.h
@@ -14,10 +14,18 @@ public:
     sf::FloatRect getBounds() const;
     sf::Vector2f getPosition() const;
 
+    void takeDamage(int amount);           
+    sf::FloatRect getAttackBounds() const; 
+    bool getIsAttacking() const;           
+    int getHp() const;                     
+
 private:
     PlayerID id;
 
     sf::RectangleShape shape;
+    // --- NUEVO: Textura para el sprite del jugador ---
+    sf::Texture texture;
+    // -------------------------------------------------
     sf::Vector2f velocity;
     sf::Vector2f position;
 
@@ -26,14 +34,22 @@ private:
     bool isDashing;
     float dashTimer;
 
+    int hp;
+    int maxHp;
+    bool isAttacking;
+    float attackTimer;
+    float facingDir; 
+
     static constexpr float SPEED = 200.f;
     static constexpr float JUMP_FORCE = -550.f;
     static constexpr float GRAVITY = 1200.f;
     static constexpr float MAX_FALL = 600.f;
     static constexpr float DASH_SPEED = 500.f;
     static constexpr float DASH_DURATION = 0.15f;
+    
+    static constexpr float ATTACK_DURATION = 0.2f; 
 
-    sf::Keyboard::Key keyLeft, keyRight, keyJump, keyDash;
+    sf::Keyboard::Key keyLeft, keyRight, keyJump, keyDash, keyAttack;
 
     void applyGravity(float dt);
     void resolveCollisions(const std::vector<sf::FloatRect>& platforms);
